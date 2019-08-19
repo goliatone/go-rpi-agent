5202aa195f740fe9d2635dac25699361c508fbb2
goliatone <burgosemi@gmail.com>
2019-08-19 14:00:01 -0700

    Update main file

--- main.go
+++ main.go
@@ -32,10 +32,11 @@ var VERSION string
 var BUILD_DATE string
 
 var (
-	name    = flag.String("name", "rpi-agent", "Set the agents name, default is rpi-agent.")
-	domain  = flag.String("domain", "local", "Set the search domain. For local networks, default is fine.")
-	service = flag.String("service", "_rpi._tcp", "Set the service category to look for devices.")
-	port    = flag.Int("port", 8080, "Service port.")
+	name   	 	= flag.String("name", "rpi-agent", "Set the agents name, default is rpi-agent.")
+	domain  	= flag.String("domain", "local", "Set the search domain. For local networks, default is fine.")
+	service		= flag.String("service", "_rpi._tcp", "Set the service category to look for devices.")
+	port    	= flag.Int("port", 8080, "Service port.")
+	registryUrl = flag.String("registry", "", "Registry service URL.")
 )
 
 func main() {
@@ -86,6 +87,149 @@ func main() {
 	log.Println("Sutting down service...")
 }
 
+// HTTP service to introspect RPi instance
+func startService(deviceUUID string) {
+	host, _ := os.Hostname()
+	metadata, err := getAddress()
+	handleError(err, "Error GetAddress: ")
+
+	serial, err := getSerial()
+	handleError(err, "Error GetSerial: ")
+
+	metadata["serial"] = serial
+
+	machineId, err := getMachineID()
+	handleError(err, "Error GetMachineID: ")
+
+	metadata["machine-id"] = machineId
+
+	startTime := time.Now()
+	// metadata["agent_start"] = startTime.Unix()
+	metadata["agent_start"] = startTime.Format(time.RFC3339)
+
+	metadata["agent_uptime"] = time.Since(startTime)
+
+	metadata["hostname"] = host + ".local"
+
+	mac, err := getMac("wlan0")
+	metadata["mac_wlan0"] = mac
+
+	mac, err = getMac("eth0")
+	metadata["mac_eth0"] = mac
+
+	output := make(map[string]interface{})
+
+	output["metadata"] = metadata
+	output["uuid"] = deviceUUID
+	output["name"] = getNameFromHostname(host, "")
+	output["status"] = "online"
+	output["alias"] = serial
+
+	if registryUrl != nil {
+		jsonData := json.Marshal(output)
+		post(registryUrl, jsonData)
+	}
+
+	http.HandleFunc("/", func(rw http.ResponseWriter, r *http.Request) {
+		//Update our uptime
+		metadata["agent_uptime"] = time.Now().Sub(startTime)
+
+		rw.Header().Set("Content-Type", "application/json")
+		json.NewEncoder(rw).Encode(output)
+	})
+
+	p := fmt.Sprintf(":%d", *port)
+	log.Println("Starting HTTP instrospection service...")
+	log.Printf("Service available at: %s%s", host, p)
+
+	if err := http.ListenAndServe(p, nil); err != nil {
+		log.Fatal(err)
+	}
+}
+
+func getNameFromHostname(hostname string, prefix string) string {
+	hostname = strings.TrimSuffix(hostname, ".local")
+	return prefix + hostname
+}
+
+
+
+func handleError(err error, msg string) {
+	if err != nil {
+		os.Stderr.WriteString(msg + err.Error() + "\n")
+		os.Exit(1)
+	}
+}
+
+func post(url string, jsonData []byte) (*http.Response, error) {
+	req, err := http.NewRequest("POST", url, bytes.NewBuffer(jsonData))
+
+	if err != nil {
+		return nil, err
+	}
+
+	req.Header.Set("Content-Type", "application/json")
+
+	client := &http.Client{}
+	resp, err := client.Do(req)
+	if err != nil {
+		return nil, err
+	}
+	defer resp.Body.Close()
+	return resp, nil
+}
+
+//machine-id is found in either /etc/machine-id or
+// /var/lib/dbus/machine-id 
+func getMachineID() (string, error) {
+
+	func readMachineID(filepath string) (string, error) {
+		contents, err := ioutil.ReadFile(filepath)	
+		if err != nil {
+			return "", err
+		}
+		return string(contents), nil
+	}
+
+	contents, err := readMachineID("/var/lib/dbus/machine-id")
+	
+	if err != nil {
+		return "", err
+	}
+
+	if contents != "" {
+		return contents
+	}
+
+	return readMachineID("/etc/machine-id")
+}
+
+func getAddress() (map[string]interface{}, error) {
+	output := make(map[string]interface{})
+	inter, err := net.Interfaces()
+	if err != nil {
+		return nil, err
+	}
+
+	for _, ifa := range inter {
+		addrs, err := ifa.Addrs()
+
+		if err != nil {
+			return nil, err
+		}
+
+		for _, a := range addrs {
+			if ipnet, ok := a.(*net.IPNet); ok && !ipnet.IP.IsLoopback() {
+				if ipnet.IP.To4() != nil {
+					output["ip_"+string(ifa.Name)] = ipnet.IP.String()
+				}
+			}
+		}
+	}
+
+	return output, nil
+}
+
 func getDefaultUUID() string {
 	if dat, _ := ioutil.ReadFile(uuidFile); dat != nil {
 		uuid := string(dat)
@@ -128,110 +272,4 @@ func getMac(iface string) (string, error) {
 	}
 	mac := strings.Replace(string(contents), "\n", "", -1)
 	return mac, nil
-}
-
-// HTTP service to introspect RPi instance
-func startService(deviceUUID string) {
-	host, _ := os.Hostname()
-	metadata, err := getAddress()
-	handleError(err, "Error GetAddress: ")
-
-	serial, err := getSerial()
-	handleError(err, "Error GetSerial: ")
-
-	metadata["serial"] = serial
-
-	startTime := time.Now()
-	// metadata["agent_start"] = startTime.Unix()
-	metadata["agent_start"] = startTime.Format(time.RFC3339)
-
-	metadata["agent_uptime"] = time.Since(startTime)
-
-	metadata["hostname"] = host + ".local"
-
-	mac, err := getMac("wlan0")
-	metadata["mac_wlan0"] = mac
-
-	mac, err = getMac("eth0")
-	metadata["mac_eth0"] = mac
-
-	output := make(map[string]interface{})
-
-	output["metadata"] = metadata
-	output["uuid"] = deviceUUID
-	output["name"] = getNameFromHostname(host, "")
-	output["status"] = "online"
-	output["alias"] = serial
-
-	http.HandleFunc("/", func(rw http.ResponseWriter, r *http.Request) {
-		//Update our uptime
-		metadata["agent_uptime"] = time.Now().Sub(startTime)
-
-		rw.Header().Set("Content-Type", "application/json")
-		json.NewEncoder(rw).Encode(output)
-	})
-
-	p := fmt.Sprintf(":%d", *port)
-	log.Println("Starting HTTP instrospection service...")
-	log.Printf("Service available at: %s%s", host, p)
-
-	if err := http.ListenAndServe(p, nil); err != nil {
-		log.Fatal(err)
-	}
-}
-
-func getNameFromHostname(hostname string, prefix string) string {
-	hostname = strings.TrimSuffix(hostname, ".local")
-	return prefix + hostname
-}
-
-func handleError(err error, msg string) {
-	if err != nil {
-		os.Stderr.WriteString(msg + err.Error() + "\n")
-		os.Exit(1)
-	}
-}
-
-func post(url string, jsonData []byte) (*http.Response, error) {
-	req, err := http.NewRequest("POST", url, bytes.NewBuffer(jsonData))
-
-	if err != nil {
-		return nil, err
-	}
-
-	req.Header.Set("Content-Type", "application/json")
-
-	client := &http.Client{}
-	resp, err := client.Do(req)
-	if err != nil {
-		return nil, err
-	}
-	defer resp.Body.Close()
-	return resp, nil
-}
-
-func getAddress() (map[string]interface{}, error) {
-	output := make(map[string]interface{})
-	inter, err := net.Interfaces()
-	if err != nil {
-		return nil, err
-	}
-
-	for _, ifa := range inter {
-		addrs, err := ifa.Addrs()
-
-		if err != nil {
-			return nil, err
-		}
-
-		for _, a := range addrs {
-			if ipnet, ok := a.(*net.IPNet); ok && !ipnet.IP.IsLoopback() {
-				if ipnet.IP.To4() != nil {
-					output["ip_"+string(ifa.Name)] = ipnet.IP.String()
-				}
-			}
-		}
-	}
-
-	return output, nil
 }
